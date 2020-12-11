bc8dcc9f70c04683cb09f2d000261db7453e3140
Denis Martin Budinski <budinski.denis@gmail.com>
2020-12-11 21:21:25 +0100

    Finished project

--- index.js
+++ index.js
@@ -2,16 +2,19 @@
 
 const fs = require('fs');
 const chalk = require('chalk');
+const path = require('path');
 
 const { lstat } = fs.promises;
 
-fs.readdir(process.cwd(), async (err, filenames) => {
+const targetDir = process.argv[2] || process.cwd();
+
+fs.readdir(targetDir, async (err, filenames) => {
     if (err) {
         console.log(err);
     }
 
     const statPromises = filenames.map(filename => {
-        return lstat(filename);
+        return lstat(path.join(targetDir, filename));
     });
 
     const allStats = await Promise.all(statPromises);
@@ -22,7 +25,7 @@ fs.readdir(process.cwd(), async (err, filenames) => {
         if(stats.isFile()){
             console.log(chalk.blue(filenames[index]));
         } else {
-            console.log(chalk.bold(filenames[index]));
+            console.log(chalk.red(filenames[index]));
         }
     }
 });
